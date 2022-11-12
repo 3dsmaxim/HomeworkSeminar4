1ac9a7d820a1d51eba31d15fb87e1b2e52471243
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-11-12 11:28:59 +0300

    Задача 29

---
+++ Task_029/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void array(int l)
+{
+    int[] arr = new int[l];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next();
+    }
+   Console.WriteLine($"Массив из {l} элементов [{String.Join(", ", arr)}]");
+  
+
+}
+
+array(5);
+Console.WriteLine();
+array(3);
+Console.WriteLine();
+array(8);
+Console.WriteLine();
